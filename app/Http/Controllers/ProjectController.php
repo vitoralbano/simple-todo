591c7c361549b52bae1575208f20245d3ead25f3
Vitor Albano <vitoralbano@gmail.com>
2020-05-27 08:51:15 -0300

    ProjectControler base

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -7,6 +7,10 @@ use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware(['auth']);
+    }
     /**
      * Display a listing of the resource.
      *
@@ -14,7 +18,10 @@ class ProjectController extends Controller
      */
     public function index()
     {
-        //
+        $projects = [];
+        return view('projects.index', [
+            'projects' => $projects
+        ]);
     }
 
     /**
