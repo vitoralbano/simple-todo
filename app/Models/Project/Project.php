506c11835ae8168b11d9e8477adedaef28a77ea0
Vitor Albano <vitoralbano@gmail.com>
2020-05-27 08:49:24 -0300

    defining models

--- app/Models/Project/Project.php
+++ app/Models/Project/Project.php
@@ -6,5 +6,25 @@ use Illuminate\Database\Eloquent\Model;
 
 class Project extends Model
 {
-    //
+    protected $fillable = ['name', 'description', 'user_id'];
+    
+    public function owner()
+    {
+        return $this->belongsTo('App\User');
+    }
+
+    public function parentProject()
+    {
+        return $this->belongsTo('App\Models\Project\Project');
+    }
+
+    public function subProjects()
+    {
+        return $this->hasMany('App\Models\Project\Project');
+    }
+
+    public function tasks()
+    {
+        return $this->hasMany('App\Models\Task\Task');
+    }
 }
--- app/Models/Task/Task.php
+++ app/Models/Task/Task.php
@@ -2,9 +2,21 @@
 
 namespace App\Models\Task;
 
+use App\Models\Project\Project;
+use App\User;
 use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    //
+    protected $fillable = ['user_id', 'done_by_user_id', 'value', 'done_at', 'archived'];
+    
+    public function project()
+    {
+        return $this->belongsTo('App\Models\Project\Project');
+    }
+
+    public function doneBy()
+    {
+        return $this->belongsTo(User::class, 'done_by_user_id');
+    }
 }
